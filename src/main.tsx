68eadace00030d1b038d15d4d778ec9c722977ac
Mksym Bondarenko <maksym.bondarenko@eliftech.com>
2025-04-12 13:56:02 +0000

    Added news creation form, added react-hook-form, react-query and yup validation

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { Theme } from "@radix-ui/themes";
-import { ToasterProvider } from "./components/theme/ToasterProvider";
-import { AppRouter } from "./router/AppRouter";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Theme } from '@radix-ui/themes';
+import { ToasterProvider } from './components/theme/ToasterProvider';
+import { AppRouter } from './router/AppRouter';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './queries';
 function App() {
-   return (
-      <Theme
-         appearance="dark"
-         accentColor="crimson"
-         radius="medium"
-         panelBackground="translucent"
-      >
-         <AppRouter />
-         <ToasterProvider />
-      </Theme>
-   );
+  return (
+    <Theme
+      appearance="dark"
+      accentColor="crimson"
+      radius="medium"
+      panelBackground="translucent">
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <ToasterProvider />
+      </QueryClientProvider>
+    </Theme>
+  );
 }
 
-createRoot(document.getElementById("root")!).render(
-   <StrictMode>
-      <App />
-   </StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
---
+++ src/queries/index.ts
@@ -0,0 +1,5 @@
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
+export * from './news/createNews';
+export * from './shelters/getShelters';
